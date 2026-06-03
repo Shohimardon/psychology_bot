1ca79ad99ac372ae7e419f1c34bda4029bf952cc
Shohdollar <96392038+Shohimardon@users.noreply.github.com>
2026-06-04 00:23:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -68,5 +68,6 @@ WELCOME_TEXT = (
     "• Kurslar va narxlar haqida bilib olishingiz\n"
     "• Ro'yxatdan o'tib, onlayn to'lashingiz\n"
     "• Savol berishingiz mumkin\n\n"
-    "Qiziqtirgan bo'limni tanlang 👇" 
+    "Qiziqtirgan bo'limni tanlang 👇"
 )
+
